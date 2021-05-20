5b8170666607207bcf8f90032ace332d8150aa8c
Vincent LE GOFF <vince.legoff@gmail.com>
2021-05-20 22:18:50 +0200

    feat: add generate versions

--- src/api/controllers/wishlist.ts
+++ src/api/controllers/wishlist.ts
@@ -25,6 +25,24 @@ export const postWishlist = async (req: FastifyRequest<{ Body: wishlistV2 }>, re
   }
 };
 
+export const postWishlist2 = async (req: FastifyRequest<{ Body: wishlistV2 }>, resp: FastifyReply): Promise<void> => {
+  try {
+    const imgBuffer = await generateWishlist(req.log, req.body);
+    if (imgBuffer) {
+      return resp
+        .header('content-disposition', `attachment; filename="wishlist.png"`)
+        .type('image/png')
+        .status(200)
+        .send(imgBuffer);
+    }
+
+    return resp.status(500).send('Oops! An error has occured');
+  } catch (e) {
+    req.log.error(e);
+    return resp.status(500).send('Oops! An error has occured');
+  }
+};
+
 export const getWishlistSettings = async (_: FastifyRequest, resp: FastifyReply): Promise<void> => {
   return resp.type('application/json').status(200).send({ fonts: supportedFonts });
 };
