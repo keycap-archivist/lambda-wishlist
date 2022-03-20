f8ea0dfd711540e02deb95050bb5f2799b35a842
Vincent Le Goff <vince.legoff@gmail.com>
2022-03-20 16:36:26 +0100

    feat: add timestream (#6)

--- src/api/controllers/wishlist.ts
+++ src/api/controllers/wishlist.ts
@@ -1,12 +1,45 @@
 import { generateWishlist, supportedFonts } from '../../internal/image-processor-v2';
 import { instance } from '../../db/instance';
+import { TimestreamWriteClient, WriteRecordsCommand } from '@aws-sdk/client-timestream-write';
 
-import type { FastifyRequest, FastifyReply } from 'fastify';
+import type { _Record } from '@aws-sdk/client-timestream-write';
+import type { FastifyRequest, FastifyReply, FastifyLoggerInstance } from 'fastify';
 import type { wishlistV2 } from '../../internal/image-processor-v2';
+
+const client = new TimestreamWriteClient({ region: 'us-east-2' });
+
+async function metrics(wl: wishlistV2, log: FastifyLoggerInstance) {
+  const recs: Array<_Record> = [];
+  for (const c of wl.caps) {
+    const hCap = instance.getColorway(c.id);
+    if (!hCap) {
+      continue;
+    }
+    recs.push({
+      MeasureValue: '1',
+      MeasureValueType: 'DOUBLE',
+      MeasureName: 'wishlist-generation',
+      Time: Date.now().toString(),
+      TimeUnit: 'MILLISECONDS',
+      Dimensions: [
+        { Name: 'maker', Value: hCap.sculpt.artist.name },
+        { Name: 'sculpt', Value: hCap.sculpt.name },
+        { Name: 'colorway', Value: hCap.id }
+      ]
+    });
+  }
+  const command = new WriteRecordsCommand({ DatabaseName: 'keycap-archivist', TableName: 'wishlist', Records: recs });
+  try {
+    const res = await client.send(command);
+    log.info(`${res.RecordsIngested?.Total} Metrics properly sent`);
+  } catch (e) {
+    log.error(e, 'Error during metric push');
+  }
+}
 
 export const postWishlist = async (req: FastifyRequest<{ Body: wishlistV2 }>, resp: FastifyReply): Promise<void> => {
   try {
-    const imgBuffer = await generateWishlist(req.log, req.body);
+    const [imgBuffer] = await Promise.all([generateWishlist(req.log, req.body), metrics(req.body, req.log)]);
     if (!imgBuffer.isError) {
       if (req.headers.accept && req.headers.accept.includes('image/png')) {
         return resp
